53b13be91a03717cc5ee4b0e0a55044b2a2617a9
miikhaiil <73438524+miikhaiil@users.noreply.github.com>
2020-11-29 16:58:53 +0100

    sad
    sad

---
+++ Projekt_1/Controls/StarRating.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt_1.Controls
+{
+    /// <summary>
+    /// Interaction logic for StarRating.xaml
+    /// </summary>
+    /// 
+
+    public partial class StarRating : UserControl
+    {
+        private List<CheckBox> CheckBoxes = new List<CheckBox>();
+        public StarRating()
+        {
+            InitializeComponent();
+            CheckBoxes.Add(CheckBox1);
+            CheckBoxes.Add(CheckBox2);
+            CheckBoxes.Add(CheckBox3);
+            CheckBoxes.Add(CheckBox4);
+            CheckBoxes.Add(CheckBox5);
+        }
+
+       private void CheckBoxClick(object sender, RoutedEventArgs e)
+        {
+            
+            if(((CheckBox)e.Source).IsChecked==false)
+            {
+                int i = CheckBoxes.IndexOf((CheckBox)e.Source);
+                CheckBoxes[i].IsChecked = true;
+                i++;
+                for (; i < 5; i++)
+                {
+                    CheckBoxes[i].IsChecked = false;
+                }
+            }
+            else if(((CheckBox)e.Source).IsChecked == true)
+            {
+                int i = CheckBoxes.IndexOf((CheckBox)e.Source);
+                for (; i >= 0; i--)
+                {
+                    CheckBoxes[i].IsChecked = true;
+                }
+            }
+        }
+    }
+}
